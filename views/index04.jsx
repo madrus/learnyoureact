7c27867543b6026e7eed287bb084c03cd53ec364
madrus <madrus@gmail.com>
2016-02-27 18:37:11 +0100

    After exercise 5

--- views/index04.jsx
+++ views/index04.jsx
@@ -20,7 +20,7 @@ class TodoList extends React.Component {
     render() {
         return (
             <div className='todoList'>
-                <table style={tableStyle}>
+                <table style={ tableStyle }>
                     <tbody>
                     <Todo title='Shopping'>Milk</Todo>
                     <Todo title='Hair cut'>13:00</Todo>
@@ -36,36 +36,12 @@ let tdStyle = {
     border: '1px solid black'
 };
 
-// I am not sure getInitialState is used as it should be used
-// The two approaches are not interchangeable.
-// You should initialize state in the constructor when using ES6 classes,
-// and define the getInitialState method when using React.createClass.
 class Todo extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            checked: false
-        };
-    }
-
-/*
-    getInitialState () {
-        return {checked: false};
-    }
-*/
-
-    handleChange(e) {
-        this.setState({checked: e.target.checked});
-    }
-
     render() {
         return (
             <tr>
-                <td style={tdStyle}>
-                    <input type="checkbox" checked={this.state.checked} onChange={this.handleChange} />
-                </td>
-                <td style={tdStyle}>{this.props.title}</td>
-                <td style={tdStyle}>{this.props.children}</td>
+                <td style={ tdStyle }>{ this.props.title }</td>
+                <td style={ tdStyle }>{ this.props.children }</td>
             </tr>
         );
     }
